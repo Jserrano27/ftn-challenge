98299ac6f9911b1629f4b91d2a332fad9154deef
Joaquin Serrano <jserrano.vd@gmail.com>
2020-01-07 22:07:14 -0300

    Creating the models

---
+++ backend/src/models/userModel.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  tasks: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'task',
+    },
+  ],
+});
+
+userSchema.pre('save', function() {
+  const hashedPassword = bcrypt.hashSync(this.password, 12);
+  this.password = hashedPassword;
+});
+
+const user = mongoose.model('user', userSchema);
+
+export default user;
