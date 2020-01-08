a820a549783270004929ecd8879c257a1c70cd8b
Joaquin Serrano <jserrano.vd@gmail.com>
2020-01-07 22:09:37 -0300

    Creating the schemas

---
+++ backend/src/schemas/userSchema.js
@@ -0,0 +1,22 @@
+import { gql } from 'apollo-server';
+
+export default gql`
+  type User {
+    id: ID!
+    name: String!
+    tasks: [Task!]!
+  }
+
+  type Token {
+    token: String!
+  }
+
+  extend type Query {
+    user(id: ID!): User
+    login(email: String!, password: String!): Token!
+  }
+
+  extend type Mutation {
+    createUser(name: String!, email: String!, password: String!): User
+  }
+`;
